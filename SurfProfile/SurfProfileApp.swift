d4db69683c23f2ca6e2b7443d4c965eb7e3bf724
AndrewKurdin <exehomework@gmail.com>
2022-09-10 00:52:29 +0300

    Initial Commit

---
+++ SurfProfile/SurfProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurfProfileApp.swift
+//  SurfProfile
+//
+//  Created by Курдин Андрей on 10.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SurfProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
